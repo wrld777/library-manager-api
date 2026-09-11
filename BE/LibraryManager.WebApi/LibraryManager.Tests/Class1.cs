8e37a64543a120cd3f525b1aaaaa1dc6021dae92
ahmed <ahmedbejaoui292@gmail.com>
2026-09-12 01:10:11 +0200

    Solution .Vs

---
+++ BE/LibraryManager.WebApi/LibraryManager.Tests/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryManager.Tests
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ BE/LibraryManager.WebApi/Librarymanager.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Librarymanager.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
