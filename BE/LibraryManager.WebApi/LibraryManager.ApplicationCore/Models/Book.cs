1a4e1073bbd397f31c8dbebe468e1604d8d4644c
ahmed <ahmedbejaoui292@gmail.com>
2026-09-12 01:24:32 +0200

    Creazione Entita di riferimento

---
+++ BE/LibraryManager.WebApi/LibraryManager.ApplicationCore/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.ApplicationCore.Models
+{
+    public class Book
+    {
+        string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
+        public int CopiesAvailable { get; set; }
+    }
+}
---
+++ BE/LibraryManager.WebApi/LibraryManager.ApplicationCore/Models/Loan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.ApplicationCore.Models
+{
+    public class Loan
+    {
+        public Guid LoanId { get; set; }
+        public Guid MemberId { get; set; }
+        public Guid BookId { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
